dd82cca264d7f762419b44c14ff6edaaddf2ff39
naughtyStark <sidharthtalia@ymail.com>
2018-05-22 22:23:54 +0530

    Update complementry_filter.ino

--- Altitude_MPU6050_BMP180/complementry_filter.ino
+++ Altitude_MPU6050_BMP180/complementry_filter.ino
@@ -17,21 +17,41 @@ void complementry_filter()
   //now we have the height estimate and it's standard deviation.
   
   get_imu();
+  float dummyA = az,Accel_SD;
   az= 0.05 * az + 0.95 *az_prev;                                                                    //LOW pass filter
   az_prev=az;
+  Accel_SD = abs(dummyA - az)/az; //adhoc way of finding variance in acceleration. just take the difference between the 
+                              //low pass value and the current value. if they differ by a lot, then it means there is a lot of error
+  //this is basically like using a gaussian filter. If you're getting your exam answer scripts back and everyone up to your roll number 
+  //had their marks around 60-65 then you'd expect your marks to be near 60-65 too. if you get 10, that would indicate that there's been
+  // some problem. (if you had 90+ marks you'd probably shut up and take the sheet home with you so that the teacher can't even look at it 
+  //again).
   
-  vz_est_acc=(double) prev_vz_est + az * dt;                                                        //updating velocity estimate  from acceleration data
-  vz_est_baro=(double) (h_est_baro-prev_h_est_baro)/dt;                                             //velocity estimate from barometer..fluctuates alot
+  vz_est_acc=(double) prev_vz_est + az * dt;                  //updating velocity estimate  from acceleration data
+  vz_est_baro=(double) (h_est_baro-prev_h_est_baro)/dt;       //velocity estimate from barometer..fluctuates alot
   prev_h_est_baro= h_est_baro;
   
+  /*since we don't have the covariance matrix nonsense here, its not a true kalman filter, 
+    Psuedo Kalman gain  = error in the estimate(Accel_SD)
+                  --------------------------------
+       (error in estimate(Accel_SD) + error in measurement(baro_SD))
+  */
+  float PKG = Accel_SD/(Accel_SD + Baro_SD);
   
-  vz_est= gamma*(vz_est_acc) + (1-gamma)*(vz_est_baro);                                                //complmentry filter
-  vz_est= 0.05*vz_est + 0.95*prev_vz_est;                                                             //LOW pass filter
+  vz_est= (1-PKG)*(vz_est_acc) + PKG*(vz_est_baro);           //Psuedo Kalman filter
+  vz_est= 0.05*vz_est + 0.95*prev_vz_est;                   //LOW pass filter
   prev_vz_est=(double)vz_est;
-  
-  h_est_acc=(double) prev_h_est + prev_vz_est * dt + 0.5*az*dt*dt;                                    //s= s_prev+ ut+ 0.5 a dt^2
-  h_est_acc= 0.05*h_est_acc + 0.95*prev_h_est_acc;                                                    //LOW pass filter
+  // h_est_acc=(double) prev_h_est + prev_vz_est * dt + 0.5*az*dt*dt;  this line is gave me cancer.
+  /*
+    We just put so much effort into filtering the vertical velocity and now you're bringing this unfiltered scum (az) back 
+    into the equation. Why bro. 
+  */
+  h_est_acc=(double) prev_h_est + vz_est * dt;                                    //s= s_prev + new_velocity*dt  
+  h_est_acc= 0.05*h_est_acc + 0.95*prev_h_est_acc;                              //LOW pass filter
   prev_h_est_acc=h_est_acc;
+  
+  /*look, I already used up Accel_SD. so lemme use
+  */
   
   h_est=(double) beta*(h_est_acc) + (1-beta)*(h_est_baro);                                            //complemntry filter
   prev_h_est=(double)h_est;
